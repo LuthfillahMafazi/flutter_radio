cdb331ae7917a75d2e09a8d37a9b5d6d212f4e0f
LuthfillahMafazi <luthfimafazi18@gmail.com>
2021-02-09 20:24:00 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/zii/flutter_radio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zii.flutter_radio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_radio/radio_app.dart';
+
+void main() => runApp(RadioApp());
